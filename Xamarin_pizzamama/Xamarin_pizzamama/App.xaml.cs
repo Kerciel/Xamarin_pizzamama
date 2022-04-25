9692597e2f72ef79a4f3319799573d05e2727974
Fazul ABDOU <abdou.fazul.97615@gmail.com>
2022-04-25 16:17:42 +0200

    START PROJET

--- Xamarin_pizzamama/Xamarin_pizzamama/App.xaml.cs
+++ Xamarin_pizzamama/Xamarin_pizzamama/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin_pizzamama.views;
 
 namespace Xamarin_pizzamama
 {
@@ -10,7 +11,7 @@ namespace Xamarin_pizzamama
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new Listespizza());
         }
 
         protected override void OnStart()
