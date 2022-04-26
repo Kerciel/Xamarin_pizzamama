8e9bb238d9bd312746b6d61d7207906c50aeaf5b
Fazul ABDOU <abdou.fazul.97615@gmail.com>
2022-04-26 14:19:41 +0200

    realisation de la partie communication avec l'api getpizzas du site

--- Xamarin_pizzamama/Xamarin_pizzamama.Android/Resources/Resource.designer.cs
+++ Xamarin_pizzamama/Xamarin_pizzamama.Android/Resources/Resource.designer.cs
@@ -16652,22 +16652,28 @@ namespace Xamarin_pizzamama.Droid
 			public const int notify_panel_notification_icon_bg = 2131165323;
 			
 			// aapt resource value: 0x7F07008C
-			public const int pizza_logo = 2131165324;
+			public const int pizza_alert = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int preference_list_divider_material = 2131165325;
+			public const int pizza_block = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int splash = 2131165326;
+			public const int pizza_logo = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int test_custom_background = 2131165327;
+			public const int preference_list_divider_material = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int tooltip_frame_dark = 2131165328;
+			public const int splash = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int tooltip_frame_light = 2131165329;
+			public const int test_custom_background = 2131165329;
+			
+			// aapt resource value: 0x7F070092
+			public const int tooltip_frame_dark = 2131165330;
+			
+			// aapt resource value: 0x7F070093
+			public const int tooltip_frame_light = 2131165331;
 			
 			static Drawable()
 			{
--- Xamarin_pizzamama/Xamarin_pizzamama/Model/Pizza.cs
+++ Xamarin_pizzamama/Xamarin_pizzamama/Model/Pizza.cs
@@ -4,16 +4,35 @@ using System.Text;
 
 namespace Xamarin_pizzamama.Model
 {
+    //Creation d'une classe pizza pour les donnée
     public  class Pizza
     {
-        public string nom { get; set; }
-        public int prix { get; set; }
-        public string imageURL { get; set; }
-        public List<string> ingredients { get; set; }
-        
-        public string prixEuro { get { return prix + "€"; } }
-        public string ingredientStr { get { return String.Join(",", ingredients); } }
-        
+        //{nom, prix, imageURL, ingredients}
+         public string nom { get; set; }
+         public int prix { get; set; }
+         public string imageURL { get; set; }
+         public List<string> ingredients { get; set; }
+
+         //prixEuro(€) 
+         public string prixEuro { get { return prix + "€"; } }
+
+         //ingredientStr(string)
+         public string ingredientStr { get { return String.Join(",", ingredients); } }
+
+        /* *************************************************Partie dynamique****************************************************
+        public string nom ;
+        public string prix;
+        public string ingrédients;
+
+        public Pizza(string nom, int prix, List<string> ingredients)
+        {
+            this.nom = nom;
+            this.prix = prix + "€";
+            this.ingrédients = String.Join(",",ingredients);
+        }
+
+        */
+
 
     }
 }
